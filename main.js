4e42af2981fef8173394bddfd7c0338bbca9c59d
Macbook Air <lautarostrappazzon@gmail.com>
2022-07-19 19:13:22 -0300

    Projects arrow scrolling functionality bugs fixed

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 const buttonRight = document.querySelector('.right');
 const buttonLeft = document.querySelector('.left');
+const scrollContainer = document.querySelector('.projects__horizontal-scroll-container')
+
+let scrollCounter = 0;
+console.log(scrollCounter)
+
+const scrollLocation = () => {
+    if (scrollCounter == 0) {
+        buttonLeft.style.display = 'none';
+        buttonRight.style.display = 'block';
+    }
+    if (scrollCounter == 2){
+        buttonRight.style.display = 'none';
+        buttonLeft.style.display = 'block';
+
+    }
+    if (scrollCounter > 0 && scrollCounter < 2) {
+        buttonLeft.style.display = 'block';
+        buttonRight.style.display = 'block';
+    }
+}
+// scrollLocation()
 
 buttonRight.onclick = function () {
-    document.querySelector('.projects__horizontal-scroll-container')
-        .scrollLeft += 756;
+    scrollContainer.scrollLeft += 754;
+    scrollCounter += 1;
+    scrollLocation()
+    console.log(scrollCounter)
 };
 buttonLeft.onclick = function () {
-    document.querySelector('.projects__horizontal-scroll-container')
-        .scrollLeft -= 756;
+    scrollContainer.scrollLeft -= 754;
+    scrollCounter -= 1;
+    scrollLocation()
+    console.log(scrollCounter)
 };
